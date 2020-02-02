b2b2b6efd45d9cdc5680ae0eba3f56847de0f433
核心力量 <kernelforce@gmail.com>
2020-02-02 16:15:50 +0800

    使用UUID3生成临时文件名
    以规避中文文件名导致的异常
    统一指定临时目录
    返回结果中增加TempFile字段

--- app.py
+++ app.py
@@ -19,26 +19,34 @@ from application.invoice_e import invoice_e
 from application.invoice_m import invoice_m
 import pytz
 port = 11111
-allowed_extension = ['jpg','png','JPG']
+allowed_extension = ['jpg','png']
+temp_dir = "temp"
 
 # Flask
 app = Flask(__name__)
 CORS(app, resources=r'/*')
 
 # 构建接口返回结果
-def build_api_result(code, message, data,file_name,ocr_identify_time):
+def build_api_result(code, message, data, file_name, temp_file, ocr_identify_time):
     result = {
         "code": code,
         "message": message,
         "data": data,
         "FileName": file_name,
+        "TempFile": temp_file,
         "ocrIdentifyTime": ocr_identify_time
     }
     return jsonify(result)
 
+def get_file_suffix(filename):
+    if '.' in filename:
+        return filename.rsplit('.', 1)[1]
+    else:
+        return None
+
 # 检查文件扩展名
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extension
+# def allowed_file(filename):
+#     return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extension
 
 
 # 增值税发票OCR识别接口
@@ -46,18 +54,21 @@ def allowed_file(filename):
 def invoice_ocr():
     # 校验请求参数
     if 'file' not in request.files:
-        return build_api_result(101, "请求参数错误", {},{},{})
+        return build_api_result(101, "请求参数错误", {}, {}, {}, {})
 
     # 获取请求参数
     file = request.files['file']
-    invoice_file_name = file.filename
+    source_file_name = file.filename.strip('"')
+    source_file_suffix = get_file_suffix(source_file_name).lower()
     
     # 检查文件扩展名
-    if not allowed_file(invoice_file_name):
-        return build_api_result(102, "失败，文件格式问题", {},{},{})
-   
-    upload_path = "test"
-    whole_path = os.path.join(upload_path,invoice_file_name)
+    if source_file_suffix not in allowed_extension:
+        return build_api_result(102, "失败，文件格式问题", source_file_name, {}, {}, {})
+
+    invoice_file_name = str(uuid.uuid3(uuid.NAMESPACE_URL, source_file_name))+"."+source_file_suffix
+
+    upload_path = temp_dir
+    whole_path = os.path.join(upload_path, invoice_file_name)
     file.save(whole_path)
     
     #去章处理方法
@@ -65,7 +76,7 @@ def invoice_ocr():
         img = cv2.imread(path,cv2.IMREAD_COLOR)
         B_channel,G_channel,R_channel=cv2.split(img)     # 注意cv2.split()返回通道顺序
         _,RedThresh = cv2.threshold(R_channel,170,355,cv2.THRESH_BINARY)
-        cv2.imwrite('./test/RedThresh_{}.jpg'.format(invoice_file_name),RedThresh)
+        cv2.imwrite('./{}/nostamp_{}'.format(temp_dir, invoice_file_name), RedThresh)
     
     def Recognition_invoice(path):
         '''
@@ -74,7 +85,7 @@ def invoice_ocr():
         :return: 发票类别
         '''
         remove_stamp(path,invoice_file_name)
-        img1 = './test/RedThresh_{}.jpg'.format(invoice_file_name)
+        img1 = './{}/nostamp_{}'.format(temp_dir, invoice_file_name)
         img1 = cv2.imread(img1)
         result_type = OCR(img1)
         result_type = union_rbox(result_type, 0.2)
@@ -105,25 +116,27 @@ def invoice_ocr():
         elif len(result_type)==0:
             return 2
     
-    Recognition_invoice = Recognition_invoice(whole_path)
+    recognition_invoice = Recognition_invoice(whole_path)
     img = cv2.imread(whole_path)
     h, w = img.shape[:2]
-    if Recognition_invoice == 1:
+    if recognition_invoice == 1:
         result = ocr_E(img)
         res = invoice_e(result)
         res = res.res
-    elif Recognition_invoice == 2:
+    elif recognition_invoice == 2:
         result = ocr_M(img)
         res = invoice_m(result)
         res = res.res
     else:
         res = []
+    
     if len(res) > 0:
         tz = pytz.timezone('Asia/Shanghai') #东八区
-        ocr_identify_time = datetime.fromtimestamp(int(time.time()),pytz.timezone('Asia/Shanghai')).strftime('%Y-%m-%d %H:%M:%S')
-        return build_api_result(100, "识别成功" , res, invoice_file_name,ocr_identify_time)
+        ocr_identify_time = datetime.fromtimestamp(int(time.time()), pytz.timezone('Asia/Shanghai'))\
+            .strftime('%Y-%m-%d %H:%M:%S')
+        return build_api_result(100, "识别成功", res, source_file_name, invoice_file_name, ocr_identify_time)
     elif len(res) == 0:
-        return build_api_result(104, "识别为空！" ,{},{},{})
+        return build_api_result(104, "识别为空！", {}, source_file_name, invoice_file_name, {})
         
 if __name__ == "__main__":
     # Run
