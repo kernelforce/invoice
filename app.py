58c4e24484b3e47ed8aaf227964b372eac475227
核心力量 <kernelforce@gmail.com>
2020-02-08 15:39:07 +0800

    修改最大阈值

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def invoice_ocr():
     def remove_stamp(path, invoice_file_name):
         img = cv2.imread(path, cv2.IMREAD_COLOR)
         B_channel, G_channel, R_channel=cv2.split(img)     # 注意cv2.split()返回通道顺序
-        _, RedThresh = cv2.threshold(R_channel, 170, 355, cv2.THRESH_BINARY)
+        _, RedThresh = cv2.threshold(R_channel, 170, 255, cv2.THRESH_BINARY)
         cv2.imwrite('./{}/nostamp_{}'.format(temp_dir, invoice_file_name), RedThresh)
     
     def Recognition_invoice(path):
